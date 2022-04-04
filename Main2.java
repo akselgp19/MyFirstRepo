976cb0175fbbfceb2ca9b9383a925abf2f72487a
unknown <akselghailanputra19@gmail.com>
2022-04-04 14:28:10 +0700

    first commit

--- Main2.java
+++ Main2.java
@@ -5,13 +5,12 @@ public class Main2 {
     public static void main(String[] args) {
         Biodata b= new Biodata();
         
-        b.getFullName("Rafi Ardian Yusuf");
-        b.getPlaceBirth("Jakarta");
-        b.getDateBirth(01," Juli ",2003);
+        b.getFullName("M. Aksel Ghailan Putra");
+        b.getPlaceBirth("Bogor");
+        b.getDateBirth(19," Mei ",2003);
         b.getHobby("Sleep");
-        b.getZodiac("Cancer");
-        b.getFavFood("Indomie");
-        b.getDreams("Software Engineer");
-        
+        b.getZodiac("Taurus");
+        b.getFavFood("Rice");
+        b.getDreams("Miliarder"); 
     }
 }
